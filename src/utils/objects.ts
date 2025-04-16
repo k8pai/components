61ab44f1ee54502e13115f829b4f38addc050e40
k8pai <thek8pai@gmail.com>
2025-04-16 23:10:26 +0530

    update: included a difference function that'll include return object with different values from a and b

--- src/utils/objects.ts
+++ src/utils/objects.ts
@@ -4,6 +4,16 @@ export const filterObject = <T extends Object>(obj: T, predicate: FilterObjectPr
 	return Object.fromEntries(Object.entries(obj).filter(([key, value]) => predicate(key as keyof T, value))) as Partial<T>;
 };
 
+export const difference = <T extends Object>(a: T, b: T): Partial<T> => {
+	const diff: Partial<T> = {};
+	for (const key in a) {
+		if (a[key] !== b[key]) {
+			diff[key] = b[key];
+		}
+	}
+	return diff;
+};
+
 export default {
 	filterObject,
 };
