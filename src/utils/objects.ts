2f587cdca92369ee51a8c72dd619cd83ba298290
k8pai <thek8pai@gmail.com>
2025-04-16 23:33:48 +0530

    fix: updated options to support undefined values, union types, preferences, ignoreKeys and more

--- src/utils/objects.ts
+++ src/utils/objects.ts
@@ -4,13 +4,58 @@ export const filterObject = <T extends Object>(obj: T, predicate: FilterObjectPr
 	return Object.fromEntries(Object.entries(obj).filter(([key, value]) => predicate(key as keyof T, value))) as Partial<T>;
 };
 
-export const difference = <T extends Object>(a: T, b: T): Partial<T> => {
-	const diff: Partial<T> = {};
-	for (const key in a) {
-		if (a[key] !== b[key]) {
-			diff[key] = b[key];
+export type differenceOptions<T> = {
+	ignoreKeys?: (keyof T | string)[];
+	union?: 'ALL' | 'DISTINCT' | 'NONE';
+	preferFrom?: 'LEFT' | 'RIGHT';
+	ignoreUndefined?: boolean;
+};
+
+export const difference = <T extends Record<string, any>>(
+	a: Partial<T>,
+	b: Partial<T>,
+	options: differenceOptions<T> = { preferFrom: 'RIGHT' }
+): Partial<T> => {
+	const { ignoreKeys = [], union = 'DISTINCT', preferFrom = 'RIGHT', ignoreUndefined = true } = options;
+
+	const keys = new Set<string>();
+
+	// Determine the keys to compare based on the union option
+	if (union === 'ALL') {
+		Object.keys(a).forEach((k) => keys.add(k));
+		Object.keys(b).forEach((k) => keys.add(k));
+	} else if (union === 'DISTINCT') {
+		const aKeys = new Set(Object.keys(a));
+		const bKeys = new Set(Object.keys(b));
+		for (const k of new Set([...aKeys, ...bKeys])) {
+			if (!(aKeys.has(k) && bKeys.has(k))) {
+				keys.add(k);
+			}
 		}
+	} else {
+		Object.keys(a).forEach((k) => keys.add(k));
 	}
+
+	// Filter out the keys that are in the ignoreKeys array
+	const diff: Partial<T> = {};
+
+	for (const key of keys) {
+		// Check if the key is in the ignoreKeys array
+		if (ignoreKeys.includes(key as keyof T)) continue;
+
+		const aVal = a[key];
+		const bVal = b[key];
+
+		const different = aVal !== bVal;
+		if (!different) continue;
+
+		const value = preferFrom === 'LEFT' ? aVal : bVal;
+
+		if (ignoreUndefined && value === undefined) continue;
+
+		diff[key as keyof T] = value;
+	}
+
 	return diff;
 };
 
