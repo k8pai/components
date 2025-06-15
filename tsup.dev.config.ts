16d16e11d1f2f62bb244de237236fe6916198841
sudarsan k pai <sudarsan@tekenlight.com>
2025-06-15 16:52:26 +0530

    fix: updated changes related to compatability issues within types resolved with bumping target version to 2022

---
+++ tsup.dev.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+	entry: ['src/index.ts', 'src/test.ts', 'src/**/*.ts'],
+	format: ['esm', 'cjs'],
+	dts: false,
+	splitting: false,
+	sourcemap: true,
+	clean: true,
+	outDir: 'dist',
+	target: 'es2020',
+});
